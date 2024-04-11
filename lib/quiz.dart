0058c2da28dd39495479a7f04b9c92fd0b83cc53
dainavi.purav <dainavi.purav@cmss.in>
2024-04-11 22:47:25 +0530

    ref : added ternary operator instead of widget assignment

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -15,17 +15,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'question-screen';
     });
   }
 
@@ -34,7 +28,9 @@ class _QuizState extends State<Quiz> {
     return MaterialApp(
       home: Scaffold(
         body: GradientContainer.purple(
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
