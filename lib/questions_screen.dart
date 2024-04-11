753f1ab680cd41d786a0960106a08a9f74f33b57
dainavi.purav <dainavi.purav@cmss.in>
2024-04-12 02:54:50 +0530

    layout modified

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -28,32 +28,34 @@ class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(context) {
     final currentQuestion = questions[currentQuestionIndex];
-    return Container(
-      margin: const EdgeInsets.all(40),
+    return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: GoogleFonts.lato(
-              color: const Color.fromARGB(255, 225, 214, 246),
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 225, 214, 246),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.getShuffledAnswers().map((answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTap: () {
-                answerQuestion(answer);
-              },
-            );
-          }),
-        ],
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
